5a36b832f1690b4d5c181a66da4c443639cc5638
Cillian <G00404411@atu.ie>
2023-03-28 22:38:20 +0100

    examples

--- src/main/java/ie/atu/InsertExample.java
+++ src/main/java/ie/atu/InsertExample.java
@@ -6,20 +6,23 @@ public class InsertExample {
     public static void main(String[] args) throws SQLException {
 
         // Connect to the database
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/exampledatabase", "root", "password");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/testdatabase", "root", "root");
 
         try {
 
             // Insert a new record into the "users" table
-            PreparedStatement stmt = conn.prepareStatement("INSERT INTO users (username, password) VALUES (?, ?)");
-            stmt.setString(1, "Des");
-            stmt.setString(2, "software");
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO person (name, age, email) VALUES (?, ?, ?)");
+            stmt.setString(1, "Ellen");
+            stmt.setString(2, "100");
+            stmt.setString(3, "ellen@atu.ie");
             stmt.executeUpdate();
 
             // Insert a new record into the "emails" table, referencing the new user
-            stmt = conn.prepareStatement("INSERT INTO emails (user_id, email) VALUES (?, ?)");
+            stmt = conn.prepareStatement("INSERT INTO address (person_id, street, city, county) VALUES (?, ?, ?, ?)");
             stmt.setInt(1, getLastInsertId(conn));
-            stmt.setString(2, "des@atu.ie");
+            stmt.setString(2, "Far Away St.");
+            stmt.setString(3, "Offaly City");
+            stmt.setString(4, "Offaly");
             stmt.executeUpdate();
 
             System.out.println("Insert completed successfully.");
--- src/main/java/ie/atu/SelectExample.java
+++ src/main/java/ie/atu/SelectExample.java
@@ -9,10 +9,12 @@ public class SelectExample {
         String username = "root";
         String password = "root";
 
+        String input = "FILM_actor";
+
         // SQL statement
-        String selectSQL = "SELECT a.first_name, a.last_name, f.title FROM actor a" +
-                "JOIN FILM_ACTOR i ON a.actor_id = i.actor_id" +
-                "JOIN FILM f ON i.film_id = f.film_id" +
+        String selectSQL = "SELECT a.first_name, a.last_name, f.title FROM actor a " +
+                "JOIN " + input + " i ON a.actor_id = i.actor_id " +
+                "JOIN FILM f ON i.film_id = f.film_id " +
                 "LIMIT 20";
 
         try (Connection connection = DriverManager.getConnection(url, username, password);
@@ -20,9 +22,9 @@ public class SelectExample {
              ResultSet resultSet = statement.executeQuery(selectSQL)) {
 
             while (resultSet.next()) {
-                String fName = resultSet.getString("first name");
-                String lName = resultSet.getString("last name");
-                String title = resultSet.getString("title");
+                String fName = resultSet.getString("a.first_name");
+                String lName = resultSet.getString("a.last_name");
+                String title = resultSet.getString("f.title");
 
                 System.out.println("First Name: " + fName + ", Last name: " + lName + ", Title: " + title);
             }
